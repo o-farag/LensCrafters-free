01ca059a8aea412e218ce7337924b371d72886bc
Tawseef Hanif <tawseefhanif@gmail.com>
2023-10-22 18:57:37 -0400

    added option to pick index of refraction in the Prescription Class. Now generated files have correct material as well

--- backend/blender_script/main.py
+++ backend/blender_script/main.py
@@ -9,17 +9,20 @@ from lens import Lens, LensPrescription, Prescription
 # initialize empty scene and spawn a lens in it with given parameters
 def startup():
     bpy.app.debug_wm = True
+    bpy.context.scene.render.engine = 'CYCLES'
 
     # spawn a lens pair
     prescription = Prescription(
                    left_eye= LensPrescription(5, 0, 0),
                    right_eye= LensPrescription(-5, 0, 0),
-                   pupillary_distance= 42 # mm
+                   pupillary_distance= 42, # mm
+                   index_of_refraction= 1.3
     )
 
     prescription.generate_lens_pair(context=bpy.context)
 
     # render scene
+    # currently creates a blend file with the generated lenses
     filepath = "./sample.blend"
     bpy.ops.wm.save_mainfile(filepath=filepath)
     return
