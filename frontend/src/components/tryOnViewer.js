38e6a8719ceefc3a6ac68b40ae4d60bdad9471c2
Jessica He <jessicahe4741@gmail.com>
2024-02-05 20:35:02 -0500

    Add test frame to track
    Signed-off-by: Jessica He <jessicahe4741@gmail.com>

--- frontend/src/components/tryOnViewer.js
+++ frontend/src/components/tryOnViewer.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Text, Button, Flex } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
+import { useGLTF } from '@react-three/drei';
+import modelUrl from './generated.glb';
+import occluderUrl from '../resources/head_occluders.glb';
+import * as THREE from 'three';
 
 // import main script and neural network model from Jeeliz FaceFilter NPM package
 import { JEELIZFACEFILTER, NN_4EXPR } from 'facefilter'
@@ -10,10 +14,8 @@ import { JEELIZFACEFILTER, NN_4EXPR } from 'facefilter'
 // The helper is not minified, feel free to customize it (and submit pull requests bro):
 import { JeelizThreeFiberHelper } from './helper/JeelizThreeFiberHelper.js'
 
-
 const _maxFacesDetected = 1 // max number of detected faces
 const _faceFollowers = new Array(_maxFacesDetected)
-let _expressions = null
 
 const callbackDetect = (faceIndex, isDetected) => {
     if (isDetected) {
@@ -44,16 +46,6 @@ const callbackTrack = (detectStatesArg) => {
 
     // render the video texture on the faceFilter canvas:
     JEELIZFACEFILTER.render_video()
-
-    // get expressions factors:
-    detectStates.forEach((detectState, faceIndex) => {
-        const exprIn = detectState.expressions
-        const expression = _expressions[faceIndex]
-        expression.mouthOpen = exprIn[0]
-        expression.mouthSmile = exprIn[1]
-        expression.eyebrowFrown = exprIn[2] // not used here
-        expression.eyebrowRaised = exprIn[3] // not used here
-    })
 }
 
 export function initJeeliz() {
@@ -68,50 +60,70 @@ export function initJeeliz() {
     return JEELIZFACEFILTER.destroy
 }
 
+const Occluder = () => {
+    const gltf = useGLTF(occluderUrl, true);
+    const occluderRef = useRef();
+  
+    useEffect(() => {
+      gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+          // Make each mesh in the occluder invisible but still affect the depth buffer
+          child.material = new THREE.MeshBasicMaterial({
+            color: 0x000000, // The color won't matter since the material is invisible
+            opacity: 0,
+            depthWrite: true, // Crucial for the occluder to still affect the depth buffer
+            colorWrite: false, // Prevents the material from coloring pixels
+          });
+        }
+      });
+  
+      occluderRef.current = gltf.scene;
+    }, [gltf.scene]);
+  
+    return <primitive ref={occluderRef} object={gltf.scene} />;
+};
+
 // This mesh follows the face. put stuffs in it.
 // Its position and orientation is controlled by Jeeliz THREE.js helper
 const FaceFollower = (props) => {
     // This reference will give us direct access to the mesh
+    const glassesRef = useRef()
+    const occluderRef = useRef()
     const objRef = useRef()
+    const { scene } = useGLTF(modelUrl);
+
     useEffect(() => {
-        const threeObject3D = objRef.current
-        _faceFollowers[props.faceIndex] = threeObject3D
+        _faceFollowers[props.faceIndex] = objRef.current
     })
 
-    const mouthOpenRef = useRef()
-    const mouthSmileRef = useRef()
-    useFrame(() => {
-        if (mouthOpenRef.current) {
-            const s0 = Math.max(0.001, props.expression.mouthOpen) // should not be 0
-            mouthOpenRef.current.scale.set(s0, 1, s0)
-        }
+    // Adjust this to fit the model to the face correctly
+    const modelScale = 9.25;
+    const modelPositionOffset = [0, 0.25, -0.25]; // x, y, z
+    const occluderScale = 0.13;
+    const occluderPositionOffset = [0, -0.25, 0.75]; // x, y, z
 
-        if (mouthSmileRef.current) {
-            const s1 = Math.max(0.001, props.expression.mouthSmile) // should not be 0
-            mouthSmileRef.current.scale.set(s1, 1, s1)
+    useFrame(() => {
+        if (glassesRef.current) {
+            glassesRef.current.scale.set(modelScale, modelScale, modelScale);
+            glassesRef.current.position.set(...modelPositionOffset);
+        }
+        if (occluderRef.current) {
+            occluderRef.current.scale.set(occluderScale, occluderScale, occluderScale);
+            occluderRef.current.position.set(...occluderPositionOffset);
         }
     })
 
     return (
         <object3D ref={objRef}>
-            <mesh name="mainCube">
-                <boxGeometry args={[1, 1, 1]} />
-                <meshNormalMaterial />
-            </mesh>
-
-            <mesh ref={mouthOpenRef} rotation={[Math.PI / 2, 0, 0]} position={[0, -0.2, 0.2]}>
-                <cylinderGeometry args={[0.3, 0.3, 1, 32]} />
-                <meshBasicMaterial color={0xff0000} />
-            </mesh>
-
-            <mesh ref={mouthSmileRef} rotation={[Math.PI / 2, 0, 0]} position={[0, -0.2, 0.2]}>
-                <cylinderGeometry args={[0.5, 0.5, 1, 32, 1, false, -Math.PI / 2, Math.PI]} />
-                <meshBasicMaterial color={0xff0000} />
-            </mesh>
+            <group ref={occluderRef}>
+                <Occluder />
+            </group>
+            <group ref={glassesRef}>
+                <primitive object={scene.clone(true)} />
+            </group>
         </object3D>
     )
 }
-
 
 // fake component, display nothing
 // just used to get the Camera and the renderer used by React-fiber:
@@ -130,6 +142,7 @@ const ThreeGrabber = (props) => {
 
 const compute_sizing = () => {
     // Compute the size of the canvas as a percentage of the window size
+    // const baseFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
     const height = window.innerHeight * (50 / 100);
     const width = window.innerWidth * (50 / 100);
 
@@ -144,16 +157,6 @@ const compute_sizing = () => {
 export const TryOnViewer = (props) => {
     const navigate = useNavigate();
 
-    // init state:
-    _expressions = []
-    for (let i = 0; i < _maxFacesDetected; ++i) {
-        _expressions.push({
-            mouthOpen: 0,
-            mouthSmile: 0,
-            eyebrowFrown: 0,
-            eyebrowRaised: 0
-        })
-    }
     const [sizing, setSizing] = useState(compute_sizing())
     const [actualSizing, setActualSizing] = useState(compute_sizing())
 
@@ -218,7 +221,7 @@ export const TryOnViewer = (props) => {
                 }}
             >
                 <ThreeGrabber actualSizing={actualSizing} setActualSizing={setActualSizing} />
-                <FaceFollower faceIndex={0} expression={_expressions[0]} />
+                <FaceFollower faceIndex={0} />
             </Canvas>
             <Flex align='center' justify='space-between' bg='#e0ddd7' h='4em' w='100%' gap='sm' px='1em'>
                 <Text size='sm' fs='italic' padding='1em'>Lens material: {props.material}</Text>
